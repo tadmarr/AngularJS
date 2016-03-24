3db77e0382b5e301b92e17fa022cea74cc3bd0d7
tadmarr <tadmarr@gmail.com>
2016-03-24 15:34:51 -0400

    evening test

---
+++ Assignment/js/get.js
@@ -0,0 +1,23 @@
+(function(){
+    angular.module('employeeApp')
+      .controller('employeeController', function($scope,$http) {
+          var employeeInfo = this;
+              $scope.Test = function(){
+                  //Whenever you click this is going to be triggered
+                  console.log('WORKED');
+
+                  //You have to create a promise that some data is going to be retrieved
+                    //Store the data in an object inside the controller
+              }
+          $http({
+              method: 'GET',
+              url: 'http://localhost:8080/json/employeeInfo.json'}).then(function(data) {
+                  employeeInfo = data;
+              });
+      });
+})();
+/*
+here are two links to database and json file
+http://localhost:8080/json/employeeInfo.json
+http://localhost:5984/database/424d51bf9bc04cdef6e6264c6c001d71
+*/
