4f4eb00976ab24162f28822011567c26dc9c9271
tadmarr <tadmarr@gmail.com>
2016-03-30 17:59:34 -0400

    deleted Assignment

--- Assignment/js/employeeform.controller.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    angular.module('employeeApp')
-      .controller('formController', function($scope, employeeFactory) {
-          employeeFactory.getEmployeeInfo().then(function(response){
-              $scope.employeeInfo = response.data;
-          });
-      });
-})();
