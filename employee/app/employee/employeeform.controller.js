a8773f0ecc56360d47221293c3214f3feb28f21f
tadmarr <tadmarr@gmail.com>
2016-04-01 16:13:28 -0400

    form-controller progressing

--- employee/app/employee/employeeform.controller.js
+++ employee/app/employee/employeeform.controller.js
@@ -1,10 +1,25 @@
 (function(){
     angular.module('employeeApp')
-      .controller('formController', function($scope, employeeFactory) {
-          employeeFactory.getEmployeeInfo().then(function(response){
-              $scope.employeeInfo = response.data;
+  .controller('formController', ['$scope', function($scope) {
+      $scope.master = {};
 
-              
-          });
-      });
+       $scope.update = function(user) {
+         $scope.master = angular.copy(user);
+       };
+
+       $scope.reset = function() {
+         $scope.user = angular.copy($scope.master);
+       };
+
+       $scope.reset();
+     }]);
+
 })();
+//   .controller('formController', function($scope, employeeFactory) {
+//       employeeFactory.getEmployeeInfo().then(function(response){
+//           $scope.employeeInfo = response.data;
+//
+//
+//       });
+//   });
+// angular.module('employeeApp')
