16302925b75e91d831211392c7a019feab0be4f2
tadmarr <tadmarr@gmail.com>
2016-04-04 14:55:59 -0400

    select toggle commented out

--- employee/app/employee/employeetable.controller.js
+++ employee/app/employee/employeetable.controller.js
@@ -4,13 +4,13 @@
           employeeFactory.getEmployeeInfo().then(function(response){
               $scope.employeeInfo = response.data;
 
-             $scope.selectedEmployee = {};
-             $scope.selectEmployee = function(employee){
-                 $scope.selectedEmployee = employee;
-                 employee.isSelected=!employee.isSelected;
-                  console.log(employee.isSelected);
-             }
+            //  $scope.selectedEmployee = {};
+            //  $scope.selectEmployee = function(employee){
+            //      $scope.selectedEmployee = employee;
+            //      employee.isSelected=!employee.isSelected;
+            //       console.log(employee.isSelected);
+            //  }
 
           });
-});
+     });
 })();
