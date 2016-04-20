3a821dc4f89a2937d8f227932142df56ea89223b
tadmarr <tadmarr@gmail.com>
2016-04-20 16:32:27 -0400

    removed controllers from routes, now they populate table

--- employee/app/employee/employeeRoutes.js
+++ employee/app/employee/employeeRoutes.js
@@ -1,14 +1,14 @@
 (function(){
 angular.module('employeeApp')
     .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/route', {
-                    templateUrl: 'add-form.html',
-                    controller: 'employeeController'
-                }).when('/route2',{
-                    templateUrl: 'template-2',
-                    controller: 'employeeController'
-                }).otherwise('/ROUTE2',{
-                    redirectTo: '/'
-                });
+    $routeProvider.when('/add_employee', {
+                        templateUrl: 'app/employee/addEmployeeTemplate.html',
+                    })
+                    .when('/edit_employee', {
+                        templateUrl: 'app/employee/editEmployeeTemplate.html',
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
 }]);
 })();
